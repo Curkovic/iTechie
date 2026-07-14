e6231ac2054daecf59e92c49a38dff6136a6f4ea
curkovic <mate.curkovic1@gmail.com>
2026-07-14 19:02:20 +0200

    cron auto update 2026-07-14 19:02:20

--- neondash/neon.sh
+++ neondash/neon.sh
@@ -5,7 +5,8 @@ export SYSTEM_STATUS="ONLINE"
 export CPU_LOAD=$(top -bn2 | grep "Cpu(s)" | tail -1 | sed 's/,/./g' | awk '{print 100 - $8}')
 export RAM_USED=$(free -g | grep Mem | awk '{print $3}')
 export RAM_TOTAL=$(free -g | grep Mem | awk '{print $2}')
-
+export NET_UP=$(cat /sys/class/net/eno1/statistics/tx_bytes | awk '{print $1/1024/1024}')
+export NET_DOWN=$(cat /sys/class/net/eno1/statistics/rx_bytes | awk '{print $1/1024/1024}')
 
 #Create index.html
 
